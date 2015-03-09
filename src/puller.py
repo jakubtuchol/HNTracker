8043e70b75a6e335372cb04038a2c407ff9a023b
Jakub Tucholski <jtucholski00@gmail.com>
2015-03-08 23:18:13 -0500

    beginning refactor of api call into two functions

--- src/puller.py
+++ src/puller.py
@@ -1,9 +1,24 @@
 import requests
 
-def get_hn_front_page(search_term):
+def construct_call(search_term=None, tags=None):
+  pass
+
+def call_api(search_term, tags=None):
+  '''
+  Given search term and tags, constructs
+  and executes search query
+  '''
   # using algolia hacker news api
   base_url = 'http://hn.algolia.com/api/v1/'
-  search_params = 'search?query={term}&tags=front_page'.format(term=search_term)
+  search_params = 'search?query={term}'
+  # handling tags present
+  if tags is not None and len(tags) > 0:
+    if len(tags) == 1:
+      search_params += '&tags={tag}'.format(tag=tags[0])
+    else:
+      tag_str = ','.join(tags)
+      search_params += '&tags=({tag})'.format(tag=tag_str)
+  
   resp = requests.get(base_url + search_params)
   if resp.status_code == 200:
     json_resp = resp.json()
