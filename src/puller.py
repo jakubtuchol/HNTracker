58a8f4fefb331f6297a11eed098a501a5504b115
Jakub Tucholski <jtucholski00@gmail.com>
2015-03-08 23:49:06 -0500

    adding tests, need to figure out how to run

--- src/puller.py
+++ src/puller.py
@@ -1,31 +1,33 @@
 import requests
 
 def construct_call(search_term=None, tags=None):
-  pass
-
-def call_api(search_term, tags=None):
   '''
-  Given search term and tags, constructs
-  and executes search query
+  Given search terms and tags, constructs url to
+  be called
   '''
   # using algolia hacker news api
   base_url = 'http://hn.algolia.com/api/v1/'
-  search_params = 'search?query={term}'
+  params = 'search?query={term}'
+
   # handling tags present
   if tags is not None and len(tags) > 0:
     if len(tags) == 1:
-      search_params += '&tags={tag}'.format(tag=tags[0])
+      params += '&tags={tag}'.format(tag=tags[0])
     else:
       tag_str = ','.join(tags)
-      search_params += '&tags=({tag})'.format(tag=tag_str)
+      params += '&tags=({tag})'.format(tag=tag_str)
   
-  resp = requests.get(base_url + search_params)
-  if resp.status_code == 200:
-    json_resp = resp.json()
-    if json_resp['nbHits'] == 0:
-      print('No hits for term')
-  else:
-    print('Cannot connect to hacker news api')
+  return base_url + params
 
-if __name__ == '__main__':
-  get_hn_front_page('haskell')
+def call_api(url):
+  '''
+  Given url, executes api call and evaluates return
+  '''
+  resp = requests.get(url)
+  if resp.status_code == 200:
+    if json_resp['nbHits'] == 0:
+      return 0
+    else:
+      return json_resp['nbHits']
+  else:
+    return -1
--- test/api_test.py
+++ test/api_test.py
@@ -1,8 +1,18 @@
 import unittest
 
+class QueryTest(unittest.TestCase):
+  def test_multiple_terms(self):
+    terms = ['haskell', 'node']
+    exp_return = 'http://hn.algolia.com/api/v1/search?query=(haskell,node)'
+    self.assertEqual(construct_call(search_terms=terms), exp_return)
+  def test_basic_search(self):
+    exp_return = 'http://hn.algolia.com/api/v1/search?query=haskell'
+    self.assertEqual(construct_call('haskell'), exp_return )
+
 class APITest(unittest.TestCase):
-  class test_api_call(self):
-    pass
+  def test_basic_query(self):
+    url = 'http://hn.algolia.com/api/v1/search?query=haskell'
+    self.assertTrue(call_api(url) > 0)
 
 if __name__ == '__main__':
   unittest.main()
