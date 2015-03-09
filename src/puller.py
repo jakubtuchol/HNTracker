2beabe9048615dd042fa910808a995ead612d679
Jakub Tucholski <jtucholski00@gmail.com>
2015-03-08 20:26:36 -0500

    checkpoint

--- src/puller.py
+++ src/puller.py
@@ -1,11 +1,16 @@
 import requests
 
-def get_hn():
-  resp = requests.get('http://news.ycombinator.com')
+def get_hn_front_page(search_term):
+  # using algolia hacker news api
+  base_url = 'http://hn.algolia.com/api/v1/'
+  search_params = 'search?query={term}&tags=front_page'.format(term=search_term)
+  resp = requests.get(base_url + search_params)
   if resp.status_code == 200:
-    resp_string = resp.content.decode('utf-8')
+    json_resp = resp.json()
+    if json_resp['nbHits'] == 0:
+      print('No hits for term')
   else:
-    print('Cannot connect to hacker news')
+    print('Cannot connect to hacker news api')
 
 if __name__ == '__main__':
-  get_hn()
+  get_hn_front_page('haskell')
