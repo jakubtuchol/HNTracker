e2d6c4aee02bf4994d01f644c785d444d878a795
Jakub Tucholski <jtucholski00@gmail.com>
2015-03-09 22:17:49 -0500

    tests now working

--- test/test_puller.py
+++ test/test_puller.py
@@ -1,18 +1,19 @@
 import unittest
+import src.puller as puller
 
 class QueryTest(unittest.TestCase):
   def test_multiple_terms(self):
     terms = ['haskell', 'node']
     exp_return = 'http://hn.algolia.com/api/v1/search?query=(haskell,node)'
-    self.assertEqual(construct_call(search_terms=terms), exp_return)
+    self.assertEqual(puller.construct_call(search_terms=terms), exp_return)
   def test_basic_search(self):
     exp_return = 'http://hn.algolia.com/api/v1/search?query=haskell'
-    self.assertEqual(construct_call('haskell'), exp_return )
+    self.assertEqual(puller.construct_call('haskell'), exp_return )
 
 class APITest(unittest.TestCase):
   def test_basic_query(self):
     url = 'http://hn.algolia.com/api/v1/search?query=haskell'
-    self.assertTrue(call_api(url) > 0)
+    self.assertTrue(puller.call_api(url) > 0)
 
 if __name__ == '__main__':
   unittest.main()
