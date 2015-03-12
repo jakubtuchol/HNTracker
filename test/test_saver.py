3eed4cc97287517ffe8be649578c1283c6208f42
Jakub Tucholski <jtucholski00@gmail.com>
2015-03-12 10:00:03 -0500

    adding test track

---
+++ test/test_saver.py
@@ -0,0 +1,11 @@
+import saver
+import unittest
+
+class SaveTest(unittest.TestCase):
+    def setup(self):
+        pass
+    def test_save(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
