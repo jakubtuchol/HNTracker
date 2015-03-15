6c01e1f40fe9322074ca4b3f87c2e00bd862e09f
Jakub Tucholski <jtucholski00@gmail.com>
2015-03-14 19:09:52 -0500

    working on main and argument parsing

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,17 @@
 import argparse
+import logging
 
 if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-v', '--verbose', 
+                      help='increase logging verbosity',
+                      action='store_true')
+  args = parser.parse_args()
 
+  filepath = sys.argv[1] # figure out a less sloppy way of doing this
+  # setting up log
+  logging.basicConfig(filename='/var/log/hntracker.log',
+                      level=logging.INFO
+                      format='%(asctime)s %(message)s')
+  logging.getLogger(__name__)
+  api_caller = HNCaller(filepath, logger)
