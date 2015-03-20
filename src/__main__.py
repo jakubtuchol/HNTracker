19b529bf903b591855c82179b4c5477534bd3567
Jakub Tucholski <jtucholski00@gmail.com>
2015-03-20 17:10:09 -0500

    adding elements to main

--- src/__main__.py
+++ src/__main__.py
@@ -9,9 +9,7 @@ if __name__ == '__main__':
                       help='increase logging verbosity',
                       action='store_true')
   parser.add_argument('-c', '--conf', type=str,
-                      help='configuration file for HN api')
-  parser.add_argument('-a', '--auth', type=str,
-                      help='authorization credentials for pocket api')
+                      help='configuration file for saver and api calls')
   parser.add_argument('-l', '--log', type=str,
                       help='path for program log')
 
@@ -40,6 +38,17 @@ if __name__ == '__main__':
     helper.fatal('no configuraton file set', logger)
 
   api_caller = puller.HNCaller(args.conf, logger)
+  csv_saver = saver.HNSaver(args.conf, logger)
+  
+  logger.info('beginning api call run')
+  run_api(api_caller, csv_saver)
+  logger.info('completed api call run')
 
   # cleaning up log
+  logger.debug('closing log')
   logger.close()
+
+def run_api(caller, saver):
+  url = caller.construct_call()
+  stories = process_call(url)
+  csv_saver.save_csv(stories)
